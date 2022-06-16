37588285b4b09029f94ed02c823c3a34083ced14
Susanelizabetth <castillosusana55@gmail.com>
2022-06-16 11:58:51 -0600

    user portal

---
+++ src/utils/UserDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "susanelizabetth",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "susanelizabetth",
+  firstname: "Susan",
+  lastname: "Castillo",
+  email: "susana@castillo.com",
+};
